dd594274fc96b371910cc2aeb31a716255f597b5
Michel Pech <michelhphz@hotmail.com>
2021-01-10 21:58:25 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Navbar from './Navbar';
 import Routes from './Routes';
@@ -6,7 +8,10 @@ import Routes from './Routes';
 
 function App() {
   return (
-    <Routes/>
+    <>
+      <Routes/>
+      <ToastContainer/>
+    </>
   );
 }
 
