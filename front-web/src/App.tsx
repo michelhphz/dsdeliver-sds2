fc1d37c1d953c39a62b9905fa2f3c8fa5baf0fb1
Michel Pech <michelhphz@hotmail.com>
2021-01-10 22:40:03 -0300

    Corrections

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Navbar from './Navbar';
 import Routes from './Routes';
 
 
